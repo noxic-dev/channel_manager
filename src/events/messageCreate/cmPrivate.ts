81218df6f6ced7c541352af829221239ae8e6491
noxic-dev <snill_gutt09@hotmail.com>
2024-11-06 15:45:56 +0100

    Minor patches on cm commands

--- src/events/messageCreate/cmPrivate.ts
+++ src/events/messageCreate/cmPrivate.ts
@@ -1,10 +1,18 @@
-import { EmbedBuilder } from 'discord.js';
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} from 'discord.js';
 import * as config from '@config';
 export default async (message: any) => {
   const ownerId = config.ownerId;
   const owner = await message.guild.members.fetch(ownerId);
   const args = message.content.split(' ');
-  if (!message.content.toLowerCase().startsWith('.cm')) return;
+  let prefix;
+  if (message.client.user.id !== '1303334967949922396') prefix = '.cm';
+  else prefix = '.cm-dev';
+  if (!args[0] || args[0] !== prefix) return;
   if (!args[1]) return;
   if (message.author.id !== ownerId) return message.react('❌');
 
@@ -16,9 +24,14 @@ export default async (message: any) => {
       if (!args[2]) return message.channel.send('No code provided.');
 
       let code = args.slice(2).join(' ');
-      if (code.startsWith('```') && code.endsWith('```')) {
+      if (
+        code.startsWith('```') ||
+        (code.startsWith('```js') && code.endsWith('```'))
+      ) {
         code = code.slice(3, -3).trim();
       }
+      if (code.includes('await'))
+        await message.react('<:soonTM:1303726162668949554>');
 
       let consoleOutput = '';
       const originalConsoleLog = console.log;
@@ -74,8 +87,27 @@ export default async (message: any) => {
         }, 500);
       }
       setTimeout(() => {
-        botResponse?.delete();
-      }, 5000);
+        const actionRow1 = new ActionRowBuilder().addComponents(
+          new ButtonBuilder()
+            .setStyle(ButtonStyle.Secondary)
+            .setEmoji('<:TrashBin:1303729257821769748>')
+            .setCustomId(`DeleteEval-${botResponse.id}`)
+        );
+
+        botResponse?.edit({ components: [actionRow1] });
+
+        const filter = (i: any) => i.customId.startsWith('DeleteEval-');
+        const collector = message.channel.createMessageComponentCollector({
+          filter,
+          time: 60000,
+        });
+
+        collector.on('collect', async (i: any) => {
+          if (i.customId.split('-')[1] !== botResponse.id) return;
+          if (i.user.id !== message.author.id) return;
+          await botResponse.delete();
+        });
+      }, 50);
       break;
     }
 
