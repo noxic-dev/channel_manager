310d262f76deec5c1e4a03b404172776b52e06cd
noxic-dev <snill_gutt09@hotmail.com>
2024-11-07 23:16:31 +0100

    increased purge limit, added .env.default, removed package-lock.json

--- src/commands/purge.ts
+++ src/commands/purge.ts
@@ -1,3 +1,5 @@
+import { Message } from 'discord.js';
+
 export default {
   options: [
     {
@@ -11,17 +13,30 @@ export default {
   // @ts-ignore
   callback: async (interaction, guild, channel, config) => {
     const clearAmount = interaction.options.getNumber('amount');
-    if (clearAmount > 100)
-      throw new Error("You can't delete more than 100 messages");
+    if (clearAmount > 500)
+      throw new Error("You can't delete more than 500 messages");
 
     if (clearAmount <= 0)
       throw new Error("You can't delete less than 1 message");
 
-    await channel.bulkDelete(clearAmount);
+    function splitNumber(num: number, chunkSize: number): number[] {
+      const chunks = [];
+      while (num > 0) {
+        const part = Math.min(chunkSize, num);
+        chunks.push(part);
+        num -= part;
+      }
+      return chunks;
+    }
 
-    return interaction.reply({
-      content: `Deleted ${clearAmount} messages`,
-      ephemeral: true,
-    });
+    const splitChunks = splitNumber(clearAmount, 100);
+    for await (const chunk of splitChunks) {
+      await channel.bulkDelete(chunk);
+    }
+    return interaction
+      .reply({
+        content: `Deleted ${clearAmount} messages`,
+      })
+      .then((msg: Message) => setTimeout(() => msg.delete(), 5000));
   },
 };
