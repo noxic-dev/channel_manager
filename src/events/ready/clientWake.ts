4e17b9a88e3fa1be2db5f18f9d3852b6db0cad26
Noxic <72988858+noxic-dev@users.noreply.github.com>
2025-01-06 19:33:54 +0100

    Added startup timings

--- src/events/ready/clientWake.ts
+++ src/events/ready/clientWake.ts
@@ -1,40 +1,40 @@
-import Table from 'cli-table3';
-import { ActivityType, type Client } from 'discord.js';
-import path from 'path';
+import Table from "cli-table3";
+import { ActivityType, type Client } from "discord.js";
+import path from "path";
 
-import type { CommandArray } from '@/types/global';
-import loadCommands from '@/utils/handlers/command';
+import type { CommandArray } from "@/types/global";
+import loadCommands from "@/utils/handlers/command";
 
 let commands: CommandArray = {
   ContextCommands: [],
   PrefixCommands: [],
-  SlashCommands: []
+  SlashCommands: [],
 };
 
 const commandsTable = new Table({
-  head: ['Command', 'Success', 'Type'],
-  style: { head: ['green'] },
-  colWidths: [15, 15]
+  head: ["Command", "Success", "Type"],
+  style: { head: ["green"] },
+  colWidths: [15, 15],
 });
 
 export default async (client: Client): Promise<void> => {
   commands = await loadCommands(
-    path.join(__dirname, '../../', 'commands'),
+    path.join(__dirname, "../../", "commands"),
     client
   );
 
   const { ContextCommands, PrefixCommands, SlashCommands } = commands;
 
   ContextCommands.forEach((command) => {
-    commandsTable.push([command.name, command ? '✅' : '❌', 'Context Menu']);
+    commandsTable.push([command.name, command ? "✅" : "❌", "Context Menu"]);
   });
 
   PrefixCommands.forEach((command) => {
-    commandsTable.push([command.name, command ? '✅' : '❌', 'Prefix Command']);
+    commandsTable.push([command.name, command ? "✅" : "❌", "Prefix Command"]);
   });
 
   SlashCommands.forEach((command) => {
-    commandsTable.push([command.name, command ? '✅' : '❌', 'Slash Command']);
+    commandsTable.push([command.name, command ? "✅" : "❌", "Slash Command"]);
   });
 
   console.log(commandsTable.toString());
@@ -42,11 +42,12 @@ export default async (client: Client): Promise<void> => {
   client.user?.setPresence({
     activities: [
       {
-        name: '/info',
-        type: ActivityType.Watching
-      }
+        name: "/info",
+        type: ActivityType.Watching,
+      },
     ],
-    afk: true
+    afk: true,
   });
+  console.timeEnd("Startup");
 };
 export { commands };
--- src/source.ts
+++ src/source.ts
@@ -1,52 +1,54 @@
 // Package imports
-import 'dotenv/config';
+import "dotenv/config";
 
-import { Client } from 'discord.js';
-import path from 'path';
-import { AutoPoster } from 'topgg-autoposter';
+// time startup
+console.time("Startup");
 
-export const startupTime = '968.981ms';
-console.time('Startup');
+import { Client } from "discord.js";
+import path from "path";
+import { AutoPoster } from "topgg-autoposter";
+
+export const startupTime = "968.981ms";
 
 // Local imports
-import handleError from '@/utils/handlers/error';
-import loadEvents from '@/utils/handlers/event';
-import handleInteraction from '@/utils/handlers/interactionHandler';
-import handleOwnEvents from './localEvents/voteMessageDeleteHandler';
+import handleError from "@/utils/handlers/error";
+import loadEvents from "@/utils/handlers/event";
+import handleInteraction from "@/utils/handlers/interactionHandler";
+import handleOwnEvents from "./localEvents/voteMessageDeleteHandler";
 
 handleOwnEvents();
 
 // Local constants
 const client = new Client({
-  intents: ['Guilds', 'GuildMessages', 'MessageContent']
+  intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   setInterval(() => {
     const ap = AutoPoster(process.env.TOPPGG_TOKEN as string, client);
-    ap.on('posted', () => {
-      console.log('Posted stats to Top.gg!');
+    ap.on("posted", () => {
+      console.log("Posted stats to Top.gg!");
     });
   }, 1000 * 60 * 1);
 }
 
 // Load events
-loadEvents(client, path.join(__dirname, 'events'));
-handleInteraction(path.join(__dirname, 'interactionHandlers'), client);
+loadEvents(client, path.join(__dirname, "events"));
+handleInteraction(path.join(__dirname, "interactionHandlers"), client);
 
 // Handle errors
-process.on('unhandledRejection', (err: unknown) => {
+process.on("unhandledRejection", (err: unknown) => {
   if (err instanceof Error) {
     handleError(err);
   } else {
-    console.error('Unhandled Rejection:', err);
+    console.error("Unhandled Rejection:", err);
   }
 });
-process.on('uncaughtException', (err: Error) => {
+process.on("uncaughtException", (err: Error) => {
   handleError(err);
 });
-process.on('warning', (warning: Error) => {
-  console.warn('Warning detected:', warning.message);
+process.on("warning", (warning: Error) => {
+  console.warn("Warning detected:", warning.message);
   handleError(warning);
 });
 
