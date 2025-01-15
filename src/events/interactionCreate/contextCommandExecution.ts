f594ca5ca9d98f9db30d1a8a4eb786ba8d26ad46
noxic-dev <noxic-dev@github.com>
2025-01-15 19:31:19 +0100

    Fixed command executions, shouldn't error when running commands.

--- src/events/interactionCreate/contextCommandExecution.ts
+++ src/events/interactionCreate/contextCommandExecution.ts
@@ -1,110 +1,119 @@
-import * as config from "@config";
+import * as config from '@config';
 import {
-  ApplicationCommandType,
-  MessageFlags,
-  type MessageContextMenuCommandInteraction,
-  type PermissionsBitField,
-  type UserContextMenuCommandInteraction,
-} from "discord.js";
-import { sql } from "@/utils/connections/postgresDb";
-import { commands } from "@/events/ready/clientWake";
+    ApplicationCommandType,
+    MessageFlags,
+    type MessageContextMenuCommandInteraction,
+    type PermissionsBitField,
+    type UserContextMenuCommandInteraction,
+} from 'discord.js';
+import { sql } from '@/utils/connections/postgresDb';
+import { commands } from '@/events/ready/clientWake';
 
 export default async (
-  interaction:
-    | UserContextMenuCommandInteraction
-    | MessageContextMenuCommandInteraction
+    interaction:
+        | UserContextMenuCommandInteraction
+        | MessageContextMenuCommandInteraction
 ): Promise<unknown> => {
-  let commandShouldBeCancelled = false;
-  if (
-    interaction.commandType !== ApplicationCommandType.Message &&
-    interaction.commandType !== ApplicationCommandType.User
-  )
-    return;
+    let commandShouldBeCancelled = false;
+    if (
+        interaction.commandType !== ApplicationCommandType.Message &&
+        interaction.commandType !== ApplicationCommandType.User
+    )
+        return;
 
-  const localCommand = commands.ContextCommands.find(
-    (c) => c.name === interaction.commandName
-  );
-
-  if (!localCommand)
-    return interaction.reply({
-      content: "Unknown command! (context)",
-      flags: MessageFlags.Ephemeral,
-    });
-
-  // Check permissions
-  if (localCommand.permissions) {
-    const hasPermissions = localCommand.permissions.every((p) =>
-      (interaction.member?.permissions as Readonly<PermissionsBitField>).has(p)
-    );
-    const botHasPermissions = localCommand.permissions.every((p) =>
-      (interaction.member?.permissions as Readonly<PermissionsBitField>).has(p)
+    const localCommand = commands.ContextCommands.find(
+        (c) => c.name === interaction.commandName
     );
 
-    if (!hasPermissions)
-      return interaction.reply({
-        content: `You're missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
-          ", "
-        )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
-        flags: MessageFlags.Ephemeral,
-      });
-    if (!botHasPermissions)
-      return interaction.reply({
-        content: `I'm missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
-          ", "
-        )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
-        flags: MessageFlags.Ephemeral,
-      });
-  }
-
-  if (localCommand.databaseRequired) {
-    const disabled = true;
-
-    if (!disabled) {
-      console.log(
-        "Database is required for this command, Checking database...",
-        interaction.guild!.id
-      );
-      await sql
-        .query("SELECT * FROM features WHERE serverid = $1", [
-          interaction.guild!.id,
-        ])
-        .then((result) => {
-          if (!result.rows[0])
-            sql.query("INSERT INTO features (serverid) VALUES ($1)", [
-              interaction.guild!.id,
-            ]);
-
-          const featureKey = `allow_${localCommand.name.replace("-", "_")}`;
-          if (!result.rows[0][featureKey]) {
-            interaction.reply({
-              content:
-                "This command is disabled by the server administrators.\n-# **To enable this command, Run `/config` and click enable.** \n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)",
-              flags: MessageFlags.Ephemeral,
-            });
-            commandShouldBeCancelled = true;
-
-            return;
-          }
-
-          return;
+    if (!localCommand)
+        return interaction.reply({
+            content: 'Unknown command! (context)',
+            flags: MessageFlags.Ephemeral,
         });
-    } else {
-      /*       interaction.channel?.send({
+
+    // Check permissions
+    if (localCommand.permissions) {
+        const hasPermissions = localCommand.permissions.every((p) =>
+            (
+                interaction.member?.permissions as Readonly<PermissionsBitField>
+            ).has(p)
+        );
+        const botHasPermissions = localCommand.permissions.every((p) =>
+            (
+                interaction.member?.permissions as Readonly<PermissionsBitField>
+            ).has(p)
+        );
+
+        if (!hasPermissions)
+            return interaction.reply({
+                content: `You're missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
+                    ', '
+                )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+                flags: MessageFlags.Ephemeral,
+            });
+        if (!botHasPermissions)
+            return interaction.reply({
+                content: `I'm missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
+                    ', '
+                )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+                flags: MessageFlags.Ephemeral,
+            });
+    }
+
+    if (localCommand.databaseRequired) {
+        const disabled = true;
+
+        if (!disabled) {
+            console.log(
+                'Database is required for this command, Checking database...',
+                interaction.guild!.id
+            );
+            await sql
+                .query('SELECT * FROM features WHERE serverid = $1', [
+                    interaction.guild!.id,
+                ])
+                .then((result) => {
+                    if (!result.rows[0])
+                        sql.query(
+                            'INSERT INTO features (serverid) VALUES ($1)',
+                            [interaction.guild!.id]
+                        );
+
+                    const featureKey = `allow_${localCommand.name.replace(
+                        '-',
+                        '_'
+                    )}`;
+                    if (!result.rows[0][featureKey]) {
+                        interaction.reply({
+                            content:
+                                'This command is disabled by the server administrators.\n-# **To enable this command, Run `/config` and click enable.** \n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)',
+                            flags: MessageFlags.Ephemeral,
+                        });
+                        commandShouldBeCancelled = true;
+
+                        return;
+                    }
+
+                    return;
+                });
+        } else {
+            /*       interaction.channel?.send({
         content:
           "This command requires database, but the owner is too lazy to even consider fixing this. so we will just ignore this for now.",
       }); */
+        }
     }
-  }
 
-  // Execute command
-  if (commandShouldBeCancelled) return;
-  localCommand.callback(interaction, config).catch((err: Error) => {
-    interaction.reply({
-      content: `**Command execution cancelled,** \`${err.message}\`\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+    // Execute command
+    if (commandShouldBeCancelled) return;
+    Promise.resolve()
+        .then(() => localCommand.callback(interaction, config))
+        .catch(async (err: Error) => {
+            interaction.reply({
+                content: `**Command execution cancelled,** \`${err.message}\`\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+                flags: MessageFlags.Ephemeral,
+            });
+        });
 
-      flags: MessageFlags.Ephemeral,
-    });
-  });
-
-  return;
+    return;
 };
--- src/events/interactionCreate/slashCommandExecution.ts
+++ src/events/interactionCreate/slashCommandExecution.ts
@@ -1,104 +1,117 @@
-import * as config from "@config";
+import * as config from '@config';
 import {
-  ApplicationCommandType,
-  MessageFlags,
-  type ChatInputCommandInteraction,
-  type PermissionsBitField,
-} from "discord.js";
+    ApplicationCommandType,
+    MessageFlags,
+    type ChatInputCommandInteraction,
+    type PermissionsBitField,
+} from 'discord.js';
 
-import { commands } from "@/events/ready/clientWake";
-import { sql } from "@/utils/connections/postgresDb";
+import { commands } from '@/events/ready/clientWake';
+import { sql } from '@/utils/connections/postgresDb';
 export default async (
-  interaction: ChatInputCommandInteraction
+    interaction: ChatInputCommandInteraction
 ): Promise<unknown> => {
-  if (interaction.commandType !== ApplicationCommandType.ChatInput) return;
-  if (!interaction.inGuild())
-    return interaction.reply({
-      content: "This command can only be used within a guild!",
-      flags: MessageFlags.Ephemeral,
-    });
-
-  let commandShouldBeCancelled = false;
-
-  const localCommand = commands.SlashCommands.find(
-    (c) => c.name === interaction.commandName
-  );
-
-  if (!localCommand)
-    return interaction.reply({ content: "Unknown command!", flags: MessageFlags.Ephemeral });
-
-  // Check permissions
-  if (localCommand.permissions) {
-    const hasPermissions = localCommand.permissions.every((p) =>
-      (interaction.member?.permissions as Readonly<PermissionsBitField>).has(p)
-    );
-    const botHasPermissions = localCommand.permissions.every((p) =>
-      (interaction.member?.permissions as Readonly<PermissionsBitField>).has(p)
-    );
-
-    if (!hasPermissions)
-      return interaction.reply({
-        content: `You're missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
-          ", "
-        )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
-        flags: MessageFlags.Ephemeral,
-      });
-    if (!botHasPermissions)
-      return interaction.reply({
-        content: `I'm missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
-          ", "
-        )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
-        flags: MessageFlags.Ephemeral,
-      });
-  }
-
-  if (localCommand.databaseRequired) {
-    const disabled = true;
-
-    if (!disabled) {
-      console.log(
-        "Database is required for this command, Checking database...",
-        interaction.guild!.id
-      );
-      await sql
-        .query("SELECT * FROM features WHERE serverid = $1", [
-          interaction.guild!.id,
-        ])
-        .then((result) => {
-          if (!result.rows[0])
-            sql.query("INSERT INTO features (serverid) VALUES ($1)", [
-              interaction.guild!.id,
-            ]);
-
-          const featureKey = `allow_${localCommand.name.replace("-", "_")}`;
-          if (!result.rows[0][featureKey]) {
-            interaction.reply({
-              content:
-                "This command is disabled by the server administrators.\n-# **To enable this command, Run `/config` and click enable.** \n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)",
-              flags: MessageFlags.Ephemeral,
-            });
-            commandShouldBeCancelled = true;
-
-            return;
-          }
-
-          return;
+    if (interaction.commandType !== ApplicationCommandType.ChatInput) return;
+    if (!interaction.inGuild())
+        return interaction.reply({
+            content: 'This command can only be used within a guild!',
+            flags: MessageFlags.Ephemeral,
         });
-    } else {
-      /*       interaction.followUp({
+
+    let commandShouldBeCancelled = false;
+
+    const localCommand = commands.SlashCommands.find(
+        (c) => c.name === interaction.commandName
+    );
+
+    if (!localCommand)
+        return interaction.reply({
+            content: 'Unknown command!',
+            flags: MessageFlags.Ephemeral,
+        });
+
+    // Check permissions
+    if (localCommand.permissions) {
+        const hasPermissions = localCommand.permissions.every((p) =>
+            (
+                interaction.member?.permissions as Readonly<PermissionsBitField>
+            ).has(p)
+        );
+        const botHasPermissions = localCommand.permissions.every((p) =>
+            (
+                interaction.member?.permissions as Readonly<PermissionsBitField>
+            ).has(p)
+        );
+
+        if (!hasPermissions)
+            return interaction.reply({
+                content: `You're missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
+                    ', '
+                )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+                flags: MessageFlags.Ephemeral,
+            });
+        if (!botHasPermissions)
+            return interaction.reply({
+                content: `I'm missing permissions to run this command! \n-# Permissions: ${localCommand.permissions.join(
+                    ', '
+                )}\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+                flags: MessageFlags.Ephemeral,
+            });
+    }
+
+    if (localCommand.databaseRequired) {
+        const disabled = true;
+
+        if (!disabled) {
+            console.log(
+                'Database is required for this command, Checking database...',
+                interaction.guild!.id
+            );
+            await sql
+                .query('SELECT * FROM features WHERE serverid = $1', [
+                    interaction.guild!.id,
+                ])
+                .then((result) => {
+                    if (!result.rows[0])
+                        sql.query(
+                            'INSERT INTO features (serverid) VALUES ($1)',
+                            [interaction.guild!.id]
+                        );
+
+                    const featureKey = `allow_${localCommand.name.replace(
+                        '-',
+                        '_'
+                    )}`;
+                    if (!result.rows[0][featureKey]) {
+                        interaction.reply({
+                            content:
+                                'This command is disabled by the server administrators.\n-# **To enable this command, Run `/config` and click enable.** \n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)',
+                            flags: MessageFlags.Ephemeral,
+                        });
+                        commandShouldBeCancelled = true;
+
+                        return;
+                    }
+
+                    return;
+                });
+        } else {
+            /*       interaction.followUp({
         content:
           "This command requires database, but the owner is too lazy to even consider fixing this. so we will just ignore this for now.",
       }); */
+        }
     }
-  }
-  // Execute command
-  if (commandShouldBeCancelled) return;
-  localCommand.callback(interaction, config).catch((err: Error) => {
-    interaction.reply({
-      content: `**Command execution cancelled,** \`${err.message}\`\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
-      flags: MessageFlags.Ephemeral,
-    });
-  });
+    // Execute command
+    if (commandShouldBeCancelled) return;
+    Promise.resolve()
+        .then(() => localCommand.callback(interaction, config))
+        .catch(async (err: Error) => {
+            interaction.reply({
+                content: `**Command execution cancelled,** \`${err.message}\`\n-# [Join our support server for help!](<https://discord.gg/mhsYUgFDbM>)`,
+                flags: MessageFlags.Ephemeral,
+            });
+        });
 
-  return;
+    return;
 };
