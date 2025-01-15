599742d4da78c034b9bab29da82733671bb47dcc
noxic-dev <noxic-dev@github.com>
2025-01-15 20:24:32 +0100

    Added usage and description fields to prefix commands for a help command.

--- src/commands/prefix/kick.ts
+++ src/commands/prefix/kick.ts
@@ -1,41 +1,51 @@
-import type { Message } from "discord.js";
+import type { Message } from 'discord.js';
 
-import { PrefixCommandsOptions } from "@/enums/prefixCommandsOptionsEnums";
+import { PrefixCommandsOptions } from '@/enums/prefixCommandsOptionsEnums';
 export default {
-  permissions: ["KickMembers"],
-  commandOptions: [
-    {
-      name: "Target User",
-      description: "The user to target",
-      required: true,
-      type: PrefixCommandsOptions.userMentionable,
-      placement: 1,
+    permissions: ['KickMembers'],
+    description: 'Kick a guild member',
+    usage: 'cm kick <@username> / <userId>',
+    commandOptions: [
+        {
+            name: 'Target User',
+            description: 'The user to target',
+            required: true,
+            type: PrefixCommandsOptions.userMentionable,
+            placement: 1,
+        },
+    ],
+    callback: (message: Message, config: void, args: string[]): unknown => {
+        let mentionType = 0; // 0 = none, 1 = ID, 2 = mention
+
+        if (args[2].includes('<@')) mentionType = 2;
+        else mentionType = 1;
+
+        let member = message.mentions.members?.first();
+
+        if (mentionType === 1) {
+            const user = message.guild?.members.cache.get(args[2]);
+            if (!user)
+                return message
+                    .reply('User not found.')
+                    .then(
+                        (msg) =>
+                            new Promise(() =>
+                                setTimeout(() => msg.delete(), 5000)
+                            )
+                    );
+            if (!member) member = user;
+        }
+        if (!member) return;
+        if (
+            member.roles.highest.position >
+            message.member!.roles.highest.position
+        )
+            throw new Error(
+                'You cannot kick someone with a higher role than you.'
+            );
+        if (member.user.id === message.author.id)
+            throw new Error('You cannot kick yourself');
+
+        member.kick(args.slice(3).join(' ') || 'No reason provided');
     },
-  ],
-  callback: (message: Message, config: void, args: string[]): unknown => {
-    let mentionType = 0; // 0 = none, 1 = ID, 2 = mention
-
-    if (args[2].includes("<@")) mentionType = 2;
-    else mentionType = 1;
-
-    let member = message.mentions.members?.first();
-
-    if (mentionType === 1) {
-      const user = message.guild?.members.cache.get(args[2]);
-      if (!user)
-        return message
-          .reply("User not found.")
-          .then(
-            (msg) => new Promise(() => setTimeout(() => msg.delete(), 5000))
-          );
-      if (!member) member = user;
-    }
-    if (!member) return;
-    if (member.roles.highest.position > message.member!.roles.highest.position)
-      throw new Error("You cannot kick someone with a higher role than you.");
-    if (member.user.id === message.author.id)
-      throw new Error("You cannot kick yourself");
-
-    member.kick(args.slice(3).join(" ") || "No reason provided");
-  },
 };
--- src/commands/prefix/ping.ts
+++ src/commands/prefix/ping.ts
@@ -1,10 +1,22 @@
+import { PrefixCommandsOptions } from '@/utils/enums/prefixCommandsOptionsEnums';
 import type { Message } from 'discord.js';
 
 export default {
-  permissions: ['SendMessages'],
-  callback: (message: Message): unknown => {
-    message.reply('Pong!');
+    description: 'Replies with pong (example command)',
+    usage: 'cm ping <number, representing how many pongs>',
+    permissions: ['SendMessages'],
+    commandOptions: [
+        {
+            name: 'Pongs',
+            description: 'Amount of pongs',
+            required: true,
+            type: PrefixCommandsOptions.integer,
+            placement: 1,
+        },
+    ],
+    callback: (message: Message, config: never, args: string[]): unknown => {
+        message.reply(`Pong! *${args[2]} Times*`);
 
-    return;
-  }
+        return;
+    },
 };
